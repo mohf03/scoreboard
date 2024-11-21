0325158b4096efb4f014718934b861893979829e
mohf03 <moritz.bierstedtt@gmail.com>
2024-11-21 17:32:06 +0100

    Commit initial files

---
+++ script.js
@@ -0,0 +1,27 @@
+let homeScore = document.getElementById("home-score");
+let guestScore = document.getElementById("guest-score");
+
+function plusOneHome() {
+  homeScore.textContent = parseInt(homeScore.textContent) + 1;
+}
+
+function plusTwoHome() {
+  homeScore.textContent = parseInt(homeScore.textContent) + 2;
+}
+
+function plusThreeHome() {
+  homeScore.textContent = parseInt(homeScore.textContent) + 3;
+}
+
+function plusOneGuest() {
+  guestScore.textContent = parseInt(guestScore.textContent)+ 1;
+}
+
+function plusTwoGuest() {
+  guestScore.textContent = parseInt(guestScore.textContent) + 2;
+}
+
+function plusThreeGuest() {
+  guestScore.textContent = parseInt(guestScore.textContent) + 3;
+}
+
